1ae3ea5f2b5c297bb535d0501f91840c8c6bf014
Thomas Kolar <thomaskolar90@gmail.com>
2024-11-23 19:29:00 +0100

    define board defs in a JS asset, too

--- src/CustomScene.js
+++ src/CustomScene.js
@@ -25,43 +25,7 @@ export default class CustomScene {
   }
 
   async defineBoard() {
-    this.BOARD_DEFS = [{
-      w: 10,
-      h: 10,
-      victoryCoords: [6, 6],
-      tiles: `
-0000000000
-0112111110
-01*2*11*10
-0223111110
-01*1112110
-01112*4*20
-01113*7*30
-01*12***20
-0111123210
-0000000000
-    `.trim(),
-      // at how many open squares to advance to the next music level
-      musicThresholds: [65, 67, 70, 72, 74, 76, 79],
-    },{
-      w: 10,
-      h: 10,
-      victoryCoords: [7, 3],
-      tiles: `
-**********
-****556***
-***4*2****
-**442447**
-23*3*3****
-0225*66***
-12*4****6*
-1*23*44*31
-1111111110
-0000000000
-    `.trim(),
-      // at how many open squares to advance to the next music level
-      musicThresholds: [23, 27, 30, 38, 40, 42, 45],
-    }];
+    this.BOARD_DEFS = eval(await (await fetch("/board_defs.js")).text());
     this.curBoardDef = 0;
   }
 
