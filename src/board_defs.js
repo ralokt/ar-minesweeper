dd39cc8229c96fe8f6827b771de4d92965ef2e6b
Thomas Kolar <thomaskolar90@gmail.com>
2024-11-24 20:29:14 +0100

    added a new level

--- src/board_defs.js
+++ src/board_defs.js
@@ -20,6 +20,25 @@
 },{
   w: 10,
   h: 10,
+  victoryCoords: [4, 5],
+  tiles: `
+012*10001*
+01*2100011
+1221011100
+1*2233*100
+223***3111
+1*347*301*
+123***3111
+01*444*111
+0112*2112*
+000111002*
+  `.trim(),
+  // at how many open squares to add the respective music layer
+  musicThresholds: [13, 19, 26, 33, 62, 66, 69],
+  initialOpen: [0, 9]
+},{
+  w: 10,
+  h: 10,
   victoryCoords: [7, 3],
   tiles: `
 **********
